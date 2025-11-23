62542fd87603b6bde55e60457a54ec3ce708e5d0
mbbaet19 <mbbaet19@up.edu.ph>
2025-11-23 16:07:10 +0800

    Changes in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,17 @@
 // src/components/Footer.jsx
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faEnvelope, faPhone, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 
 const Footer = () => {
   return (
     <footer className="bg-gray-100 mt-16 py-10 text-sm text-gray-700">
       <div className="max-w-6xl mx-auto px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
         <div>
-          <h4 className="font-semibold text-green-700 mb-2">Camarcl</h4>
-          <p>Your trusted source for beautiful plants and flowers since 2023.</p>
+          <h4 className="font-semibold text-green-700 mb-2">Camarcl Plants and Flower Shop</h4>
+          <p>Your trusted partner in growing nature at home. Proudly Filipino-owned, Camarcl Plants provides quality plants, flowers, and eco-friendly gardening essentials since 2024..</p>
         </div>
 
         <div>
@@ -29,10 +33,15 @@ const Footer = () => {
         </div>
 
         <div>
-          <h4 className="font-semibold mb-2">Contact Info</h4>
-          <p>123 Garden Street, Green City, GC 12345</p>
-          <p>(555) 123-4567</p>
-          <p>hello@camarcl.com</p>
+          <h4 className="font-semibold mb-2">Contact Us</h4>
+          <p><FontAwesomeIcon icon={faLocationDot} />Brgy. Casinsin, Pakil, Laguna, Philippines</p>
+          <p><FontAwesomeIcon icon={faPhone} />: +63 977 797 2078</p>
+          <p><FontAwesomeIcon icon={faEnvelope} />: clcamar74@gmail.com</p>
+          <p><FontAwesomeIcon icon={faGlobe} />: www.camarclplants.com</p>
+          <p><FontAwesomeIcon icon={faFacebook} />: facebook.com/camarclplants</p>
+          <p><FontAwesomeIcon icon={faInstagram} />: @camarclplants</p>
+          <br></br>
+
         </div>
       </div>
 
